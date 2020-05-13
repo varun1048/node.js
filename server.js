19a98686a3eb335cadde52d5384a68bf30cf56f6
varun <59528216+varun1048@users.noreply.github.com>
2020-05-13 18:52:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,10 +19,14 @@ var topper =[]
 function output(){
     topper.sort((x,y)=>y.time - x.time)
     topper.sort((x,y)=>y.mark - x.mark)
-
+    
     console.log(topper)
 }
+
 io.on('connection',(socket)=>{
+    socket.on('disconnect',()=>{
+        --Numberofconnection
+    })
     console.log("number of connction: "+ ++Numberofconnection)
     socket.on('info',(data)=>{
         topper.push(data)
